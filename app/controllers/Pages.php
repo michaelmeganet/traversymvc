bcf9300e929a9dacb19afd2eb0aeb341f79a16f6
Truper63 <truper63@gmail.com>
2017-12-19 15:04:31 -0500

    Loading the controller from the URL

---
+++ app/controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trupe
+ * Date: 12/19/2017
+ * Time: 12:00
+ */
+
+class Pages {
+    public function __construct()
+    {
+        echo 'Pages loaded';
+    }
+}
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -13,9 +13,30 @@ class Core {
     protected $params = [];
 
     public function __construct() {
-        $this->getUrl();
+        // print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        // Look in controllers for first url value
+        if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            // If exists then set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 index
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // Instantiate controller
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl() {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
