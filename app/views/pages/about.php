3b72b802b482b59b7110c47f6b9d756463d9c9ea
Truper63 <truper63@gmail.com>
2017-12-19 15:40:48 -0500

    Loading views

---
+++ app/views/pages/about.php
@@ -0,0 +1,8 @@
+ABOUT
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trupe
+ * Date: 12/19/2017
+ * Time: 15:34
+ */
---
+++ app/views/pages/index.php
@@ -0,0 +1,8 @@
+<h1><?php echo $data['title']; ?></h1>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trupe
+ * Date: 12/19/2017
+ * Time: 15:28
+ */
