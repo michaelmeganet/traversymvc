0a0804e4546339392f3646679740f2b042f08292
Truper63 <truper63@gmail.com>
2017-12-19 09:04:10 -0500

    Initial MVC Project Setup

---
+++ app/libraries/database.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trupe
+ * Date: 12/18/2017
+ * Time: 19:06
+ */
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trupe
+ * Date: 12/18/2017
+ * Time: 19:03
+ */
+require_once '../app/bootstrap.php';
